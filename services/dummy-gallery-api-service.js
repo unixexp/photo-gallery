d4f3a1c9c32a68e2b11c9a817177b909a2a8c2b4
Yurii Rohoza <unixexp@gmail.com>
2022-10-14 11:56:36 +0300

    Add dummy photos

--- services/dummy-gallery-api-service.js
+++ services/dummy-gallery-api-service.js
@@ -13,12 +13,23 @@ export default class DummyGalleryAPIService {
 
     // MOCK photo list
     static photos = [
-        {id: 1, categoryId: 1, filename: "photo-1.png", description: "Photo description"},
-        {id: 2, categoryId: 1, filename: "photo-2.png", description: "Photo description"},
-        {id: 3, categoryId: 1, filename: "photo-3.png", description: "Photo description"},
-        {id: 4, categoryId: 1, filename: "photo-4.png", description: "Photo description"},
-        {id: 5, categoryId: 2, filename: "photo-1.png", description: "Photo description"},
-        {id: 6, categoryId: 2, filename: "photo-2.png", description: "Photo description"}
+        {id: 1, categoryId: 1, filename: "photo-1.png", name: "Photo name 1", description: "Photo description Photo description Photo description Photo description Photo description (1)"},
+        {id: 2, categoryId: 1, filename: "photo-2.png", name: "Photo name 2", description: "Photo description Photo description Photo description Photo description Photo description (2)"},
+        {id: 3, categoryId: 1, filename: "photo-3.png", name: "Photo name 3", description: "Photo description Photo description Photo description Photo description Photo description (3)"},
+        {id: 4, categoryId: 1, filename: "photo-4.png", name: "Photo name 4", description: "Photo description Photo description Photo description Photo description Photo description (4)"},
+        {id: 5, categoryId: 1, filename: "photo-5.png", name: "Photo name 5", description: "Photo description Photo description Photo description Photo description Photo description (5)"},
+        {id: 6, categoryId: 1, filename: "photo-6.png", name: "Photo name 6", description: "Photo description Photo description Photo description Photo description Photo description (6)"},
+        {id: 7, categoryId: 1, filename: "photo-7.png", name: "Photo name 7", description: "Photo description Photo description Photo description Photo description Photo description (7)"},
+        {id: 8, categoryId: 1, filename: "photo-8.png", name: "Photo name 8", description: "Photo description Photo description Photo description Photo description Photo description (8)"},
+        {id: 9, categoryId: 1, filename: "photo-9.png", name: "Photo name 9", description: "Photo description Photo description Photo description Photo description Photo description (9)"},
+        {id: 10, categoryId: 1, filename: "photo-10.png", name: "Photo name 10", description: "Photo description Photo description Photo description Photo description Photo description (10)"},
+        {id: 11, categoryId: 1, filename: "photo-11.png", name: "Photo name 11", description: "Photo description Photo description Photo description Photo description Photo description (11)"},
+        {id: 12, categoryId: 1, filename: "photo-12.png", name: "Photo name 12", description: "Photo description Photo description Photo description Photo description Photo description (12)"},
+        {id: 13, categoryId: 2, filename: "photo-1.png", name: "Photo name 1", description: "Photo description Photo description Photo description Photo description Photo description (1)"},
+        {id: 14, categoryId: 2, filename: "photo-2.png", name: "Photo name 2", description: "Photo description Photo description Photo description Photo description Photo description (2)"},
+        {id: 15, categoryId: 2, filename: "photo-3.png", name: "Photo name 3", description: "Photo description Photo description Photo description Photo description Photo description (3)"},
+        {id: 16, categoryId: 2, filename: "photo-4.png", name: "Photo name 4", description: "Photo description Photo description Photo description Photo description Photo description (4)"},
+        {id: 17, categoryId: 2, filename: "photo-5.png", name: "Photo name 5", description: "Photo description Photo description Photo description Photo description Photo description (5)"}
     ]
 
     getCategories = async () => {
