e6a29ebcb1acee5b35b1a45db32cf91f6912c84d
Yurii Rohoza <unixexp@gmail.com>
2022-12-30 12:48:38 +0200

    TODO: make redirect after user signed in

--- middleware.js
+++ middleware.js
@@ -1,16 +1,21 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth({
-    callbacks: {
-        authorized({ req, token }) {
-            if (token) return true;
+export default withAuth(
+    function middleware(req) {
+        console.log(req.nextauth.token)
+    },
+    {
+        callbacks: {
+            authorized({ token }) {
+                return token?.role === "admin"
+            }
         }
     }
-})
+)
 
 export const config = {
     matcher: [
         "/admin/:path*",
-        "/api/admin/:path*"
+        //"/api/admin/:path*"
     ]
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -16,64 +16,29 @@ export const authOptions = {
                 if (login !== "admin" || password != "pass") {
                     throw new Error("Invalid credentials")
                 } else {
-                    return {id: "1234", name: "Administrator"}
+                    return {
+                        id: "1234",
+                        name: "Administrator",
+                        role: "admin"
+                    }
                 }
             }
         })
     ],
+
+    callbacks: {
+        jwt(params) {
+            if (params.user?.role) {
+                params.token.role = params.user.role
+            }
+
+            return params.token
+        }
+    },
 
     pages: {
         signIn: "/auth/signin"
     }
-
-    /*
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: { label: "Username", type: "text" },
-                password: {  label: "Password", type: "password" }
-            },
-            authorize: async function (credentials, req) {
-                try {
-                    const res = await fetch("http://localhost:3000/api/login", {
-                        method: "POST",
-                        body: JSON.stringify(credentials),
-                        headers: { "Content-Type": "application/json" }
-                    })
-
-                    const user = await res.json()
-
-                    if (res.ok && user) {
-                        return user
-                    } else {
-                        return null
-                    }
-                } catch (e) {
-                    throw new Error(e)
-                }
-            }
-        })
-    ],
-    secret: process.env.secret,
-    callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            return true
-        },
-        async redirect({ url, baseUrl }) {
-            return baseUrl
-        },
-        async session({ session, user, token }) {
-            // console.log("session")
-            return session
-        },
-        async jwt({ token, user }) {
-            // console.log(token)
-            // console.log(user)
-            return token
-        }
-    }
-    */
 }
 
 export default NextAuth(authOptions);
